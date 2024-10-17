4a9a0dde15d86249b8b7c3cc88ef670bd388bb78
NischaySinghPanwar <nischaysinghpanwar1@gmail.com>
2024-10-17 19:48:31 +0530

    bg removed

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
 
   return (
     
-    <div className="relative w-full h-screen">
-    <Spline
+    <div className="relative bg-red-200 w-full h-screen">
+    {/* <Spline
         scene="https://prod.spline.design/iHOcdVql8AlgsAwj/scene.splinecode"
         className="absolute inset-0 -z-10" // Position Spline in the background
-    />
+    /> */}
     <div className="flex items-center justify-center ">
         <h1 className="text-5xl mt-10 text-zinc-600">DO YOU LOVE ME?</h1>
     </div>
